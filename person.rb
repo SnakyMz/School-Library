3950215647fa4073cccacfa93a0eb99f44d3588c
SnakyMz <Soban.syeed@outlook.com>
2023-10-03 02:09:31 +0530

    Add student class

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
 class Person
-  attr_reader :id :name :age
-  attr_writer :name :age
+  attr_reader :id, :name, :age
+  attr_writer :name, :age
 
-  def initialize(name = "Unknown", age, parent_permission = true)
+  def initialize(name = "Unknown", age, parent_permission: true)
     @id = rand(1..1000)
     @name = name
     @age = age
