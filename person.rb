026728959f6f0a89a4d79e14ea725da67aca06d1
SnakyMz <Soban.syeed@outlook.com>
2023-10-03 01:59:22 +0530

    Add getters and setters for person

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
 class Person
-  def initialize (name = "Unknown", age, parent_permission = true)
-    @id
-    @name
-    @age
-    @parent_permission
+  attr_reader :id :name :age
+  attr_writer :name :age
+
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @id = rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
 end
