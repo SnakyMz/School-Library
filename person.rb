a0d6f12247b99a727af5609c2672a787f60b22f1
SnakyMz <Soban.syeed@outlook.com>
2023-10-03 20:38:44 +0530

    Inherit person class from nameable

--- person.rb
+++ person.rb
@@ -1,10 +1,12 @@
+require_relative 'nameable'
+
 # Class containing data about person
-class Person
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = rand(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -20,5 +22,9 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
