1b204d8afe44d430582a3f2a5e8290d532f98051
SnakyMz <Soban.syeed@outlook.com>
2023-10-04 22:27:11 +0530

    Fix linters #2

--- student.rb
+++ student.rb
@@ -7,14 +7,10 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.add_student(self)
   end
 end
