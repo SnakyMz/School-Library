f3062fe0ebb792fa1d5cc7e0989f6270857e73be
SnakyMz <Soban.syeed@outlook.com>
2023-10-04 21:40:46 +0530

    Add relationship Classroom-Student

--- student.rb
+++ student.rb
@@ -2,12 +2,19 @@ require_relative 'person'
 
 # Class Student inherited from Person
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def assign_classroom(new_classroom)
+    @classroom = new_classroom
+    new_classroom.students.push(self)
   end
 end
