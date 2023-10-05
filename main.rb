c780212f12618127454287299c267b8524d87d0a
SnakyMz <Soban.syeed@outlook.com>
2023-10-06 04:52:43 +0530

    Fix linters #2

--- main.rb
+++ main.rb
@@ -1,17 +1,22 @@
 require_relative 'app'
 
+
+def menu
+  puts 'Please select an option'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
 def main
   app = App.new
   puts 'Welcome to School Library'
-  while true
-    puts 'Please select an option'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
+  loop do
+    menu
     print 'Enter your option: '
     option = gets.chomp.to_i
     case option
