d6ce714b985c285abda298acc65ee0b4b7219044
SnakyMz <Soban.syeed@outlook.com>
2023-10-06 05:08:27 +0530

    Fix linters #5

--- app.rb
+++ app.rb
@@ -13,40 +13,39 @@ class App
 
   def run
     loop do
-      menu
+      puts 'Welcome to School Library'
+      puts 'Please select an option'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '7 - Exit'
+      print 'Enter your option: '
       option = gets.chomp.to_i
-      case option
-      when 1
-        list_all_books
-      when 2
-        list_all_people
-      when 3
-        create_person
-      when 4
-        create_book
-      when 5
-        create_rental
-      when 6
-        list_rental
-      when 7
-        break
-      else
-        puts 'Invalid option'
-      end
+      break if option.eql?(7)
+      menu(option)
     end
   end
 
-  def menu
-    puts 'Welcome to School Library'
-    puts 'Please select an option'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-    print 'Enter your option: '
+  def menu(option)
+    case option
+    when 1
+      list_all_books
+    when 2
+      list_all_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rental
+    else
+      puts 'Invalid option'
+    end
   end
 
   def list_all_books
