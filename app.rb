af57410f8f03092d878e81e25cc3341f51a81cdb
SnakyMz <Soban.syeed@outlook.com>
2023-10-06 03:20:34 +0530

    Add create person method

--- app.rb
+++ app.rb
@@ -29,6 +29,8 @@ class App
         list_all_books
       when 2
         list_all_people
+      when 3
+        create_person
       when 7
         break
       else
@@ -47,7 +49,55 @@ class App
   def list_all_people
     puts "Library is empty" if @people.empty?
     @people.each do |person|
-      puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+      puts "[Student] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}" if person.is_a?(Student)
+      puts "[Teacher] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}" if person.is_a?(Teacher)
     end
+  end
+
+  def create_person
+    while true
+      puts "Please select an option"
+      puts "1 - Create a student"
+      puts "2 - Create a Teacher"
+      puts "3 - Back"
+      print "Enter your option: "
+      person_option = gets.chomp.to_i
+      case person_option
+      when 1
+        create_student
+      when 2
+        create_teacher
+      when 3
+        break
+      else
+        puts "Invalid option"
+      end
+    end
+  end
+
+  def create_student
+    print "Enter student name: "
+    name = gets.chomp
+    print "Enter student age: "
+    age = gets.chomp
+    print "Enter student classroom: "
+    classroom = gets.chomp
+    print "Enter student's parent permission[Y/N]: "
+    parent_permissionpermission = gets.chomp
+    parent_permission = true if parent_permission.eql?('Y' || 'y')
+    parent_permission = false if parent_permission.eql?('N' || 'n')
+    @people << Student.new(classroom, age, name)
+    puts "Student data submitted successfully"
+  end
+
+  def create_teacher
+    print "Enter teacher name: "
+    name = gets.chomp
+    print "Enter teacher age: "
+    age = gets.chomp
+    print "Enter teacher specialization: "
+    specialization = gets.chomp
+    @people << Teacher.new(specialization, age, name)
+    puts "Teacher data submitted successfully"
   end
 end
