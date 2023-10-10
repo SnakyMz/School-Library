0f1de165fa937f373dcb62f32f593d39821fb152
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-10 13:43:05 -0300

    add load_books, load_people and load_rentals

--- app.rb
+++ app.rb
@@ -35,12 +35,46 @@ class App
   end
 
   def load_books
+    def load_books
+      books = []
+      return books unless File.read("people.json") != ''
+      booksdata = File.read("books.json")
+      bookarray = JSON.parse(booksdata)
+      bookarray.each do |book|
+        books << Book.new(book['title'], book['author'])
+      end
+      books
+    end
   end
 
   def load_people
+    def load_people
+      people = []
+      return people unless File.read("people.json") != ''
+      peopledata = File.read("people.json")
+      peoplearray = JSON.parse(peopledata)
+      peoplearray.each do |person|
+        if person['class'] == 'student'
+          people << Student.new(person['classroom'], person['age'], person['name'])
+        elsif person['class'] == 'teacher'
+          people << Teacher.new(person['specialization'], person['age'], person['name'])
+        end
+      end
+      people
+    end
   end
 
   def load_rentals
+    def load_rentals
+      rentals = []
+      return rentals unless File.read("rentals.json") != ''
+      rentalsdata = File.read("rentals.json")
+      rentalsarray = JSON.parse(rentalsdata)
+      rentalsarray.each do |rental|
+        rentals << Rental.new(rental['date'], rental['person'], rental['book'])
+      end
+      rentals
+    end
   end
 
   def list_all_books
--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require_relative 'rental'
 
 # Class containing data about person
 class Person < Nameable
-  attr_accessor :name, :age, :rental
+  attr_accessor :name, :age, :rental, :parent_permission
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
