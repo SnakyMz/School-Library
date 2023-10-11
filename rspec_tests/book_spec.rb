4048f53e92090cf1e8ecbe41511c6c184df2c7c9
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-10-11 14:33:52 -0300

    add test for teacher

--- rspec_tests/book_spec.rb
+++ rspec_tests/book_spec.rb
@@ -12,7 +12,7 @@ describe  Book do
     end
     it "Creates a rental" do
       person1 = Person.new(22, 'ismael')
-      rental1 =  @book1.add_rental "20/10/20", person1
+      @book1.add_rental "20/10/20", person1
       expect(@book1.rental.length).to eq(1) 
     end
   end
